1e537f67561c421d63fb671cd3517a6e566e0023
Vynicius <vynicius.henrique15@gmail.com>
2025-11-16 19:18:32 -0300

    criando estrutura do formulario de login

--- app/login.tsx
+++ app/login.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { router } from "expo-router";
-import { Button, Card, Text } from "react-native-paper";
+import { Button, Card, Checkbox, Text, TextInput } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { loginStyles } from "../styles/login";
 import { View, Image } from "react-native";
 
 export default function SignIn() {
   const handleLogin = () => {
-    router.replace("/Home");
+    router.navigate("/Home");
   };
   return (
     <SafeAreaView style={loginStyles.screen}>
@@ -25,9 +25,35 @@ export default function SignIn() {
       <Card style={loginStyles.card}>
         <Card.Content>
           <Text style={loginStyles.loginHeader}>Faça seu login</Text>
-          <Button mode="contained" onPress={handleLogin}>
-            Testando telas
-          </Button>
+          <View style={{ marginTop: 40 }}>
+            <View style={{ marginBottom: 40 }}>
+              <Text variant="labelLarge">Digite a sua matrícula</Text>
+              <TextInput label={"Matrícula"} />
+            </View>
+            <View style={{ marginBottom: 10 }}>
+              <Text variant="labelLarge">Digite a sua senha</Text>
+              <TextInput
+                secureTextEntry
+                right={<TextInput.Icon icon="eye" />}
+                label={"Senha"}
+              />
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginBottom: 40
+              }}
+            >
+              <Text variant="bodyMedium">Lembre-se de mim</Text>
+              <Checkbox status="checked" />
+            </View>
+          </View>
+          <View>
+            <Button mode="contained" onPress={handleLogin}>
+              Entrar
+            </Button>
+          </View>
         </Card.Content>
       </Card>
     </SafeAreaView>
